946061631dcaa83d09a1516f173f7ad0d8943a51
yangobb <limanwar694@gmail.com>
2026-05-09 00:00:02 +0800

    auto-sync: 2026-05-09 00:00:02

--- sql/海外民宿/q平台数据/流量数据.sql
+++ sql/海外民宿/q平台数据/流量数据.sql
@@ -1,16 +1,13 @@
-
 select 
-    substr(dt,1,7) month 
-    ,country_name
-    ,province_name
-    ,city_name
-    ,count(distinct concat(dt,orig_device_id)) luv 
-    ,count(distinct case when home_pv > 0 then concat(dt,orig_device_id) end) huv
-    ,count(distinct case when detail_pv > 0 then concat(dt,orig_device_id) end) duv
-    ,count(distinct case when booking_pv > 0 then concat(dt,orig_device_id) end) buv
-    ,count(distinct case when order_pv > 0 then concat(dt,orig_device_id) end) ouv
-from ihotel_default.mdw_user_app_log_sdbo_di_v1
-where (dt between '2025-07-01' and '2025-08-31' or dt between '2024-07-01' and '2024-08-31')
-and user_id is not null 
-and user_id <> ''
-group by 1,2,3,4
+    distinct 
+    dt,
+    substr(dt,1,4) yr,
+    substr(dt,6,2) mon,
+    CONCAT(substr(dt,1,4),'-','wk', weekofyear(dt)) wk,
+    orig_device_id as device_id
+from hotel.dwd_flow_app_searchlist_di
+where 
+((dt between '2025-11-03' and '2025-11-16') or (dt between '2024-11-04' and '2024-11-17'))
+and user_id is not null and user_id <> ''
+and is_international=0---非国际城市
+and country_type=1---非港澳台城市
--- sql/海外民宿/q平台数据/订单数据.sql
+++ sql/海外民宿/q平台数据/订单数据.sql
@@ -1,12 +1,17 @@
 select  
     case when checkout_date between '2026-05-01' and '2026-05-05' then '2026五一'
          when checkout_date between '2025-05-01' and '2025-05-05' then '2025五一'
-    end as period_name
+         end as period_name
+    ,city_name_info
     ,count(distinct order_no) order_num
     ,sum(room_night) night
     ,sum(final_gmv) gmv
 from (
     select *
+        ,case when city_name in ('吉隆坡','东京','大阪','曼谷','京都','普吉岛','清迈','巴厘岛','巴黎','悉尼',
+            '胡志明市','伦敦','福冈','芭堤雅','新山','札幌','济州市','河内','墨尔本','哥打京那巴鲁',
+            '首尔','乔治市','罗马','釜山','香港','迪拜','那霸','新加坡','八打灵再也','雅加达',
+            '莫斯科','巴塞罗那') then city_name else '其他' end city_name_info
     from hotel.dwd_ord_wide_order_detail_di
     where (
         checkout_date between '2026-05-01' and '2026-05-05'
@@ -18,4 +23,4 @@ from (
     and (distributor_package ='非打包' or distributor_package is null) --非打包
     and buyout_type not in('免费房','广告免房','试睡免房') --非免房 
 ) a 
-group by 1
+group by 1,2
