bff2581d6ac95a0be99481bdbd96370ba602bd86
yangobb <limanwar694@gmail.com>
2026-05-09 12:00:02 +0800

    auto-sync: 2026-05-09 12:00:02

--- sql/海外民宿/q平台数据/订单数据.sql
+++ sql/海外民宿/q平台数据/订单数据.sql
@@ -17,7 +17,7 @@ from (
         checkout_date between '2026-05-01' and '2026-05-05'
         or checkout_date between '2025-05-01' and '2025-05-05'
         )
-    and is_mainland_china = 1 
+    and is_mainland_china = 0 
     and is_valid = '1'
     and order_status not in ('CANCELLED','REJECTED') --剔除取消和拒单、
     and (distributor_package ='非打包' or distributor_package is null) --非打包
